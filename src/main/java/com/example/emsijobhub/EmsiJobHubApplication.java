8eff97aa8a5686a14f60149fc4902fcb420b7723
[mehdi] <[mehdi@gmail.com]>
2024-05-29 22:42:33 +0100

    first commit

---
+++ src/main/java/com/example/emsijobhub/EmsiJobHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.emsijobhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsiJobHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsiJobHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emsijobhub/EmsiJobHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emsijobhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsiJobHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
